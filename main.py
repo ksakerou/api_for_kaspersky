fbc36cc6511995ea09c5e621e78ad2d7a5f47663
ale <i@example.com>
2023-04-23 22:54:52 +0000

    Just architecture

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def hello():
-	return "Hello"
+st = False
+
+@app.get("/api/pn")
+def status_proc():
+	if st:
+		return "running"
+	else:
+		return "stopped"
+
+@app.get("/api/pn/result")
+def result_proc():
+	return "result"
+
+@app.post("/api/pn")
+def start_proc(action: str):
+	if action == "start":
+		st = True
+		return "running"
+	elif action == "stop":
+		st = False
+		return "stopped"
+	else:
+		return "404"

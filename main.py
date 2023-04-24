4a84433b9f692839ce19836a31fa101d85ab700c
ale <i@example.com>
2023-04-24 00:21:47 +0000

    Minimal valuable product:)

--- main.py
+++ main.py
@@ -2,28 +2,38 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 
+import subprocess
+
 app = FastAPI()
 
-st = False
+pr = subprocess.Popen('true')
+#subprocess.Popen(['ping', 'google.com'], stdout=subprocess.PIPE)
 
 @app.get("/api/pn")
 def status_proc():
-	if st:
+	global pr
+	if pr.poll() is None:
 		return "running"
 	else:
 		return "stopped"
 
 @app.get("/api/pn/result")
 def result_proc():
+	global pr
+	if pr.poll() is not None:
+		return pr.stdout
 	return JSONResponse(status_code=404,content="")
 
 @app.post("/api/pn")
 def start_proc(action: str):
-	if action == "start":
-		st = True
-		return "running"
+	global pr
+	if action == "start" and pr.poll() is None:
+		return "already runnning"
+	elif action == "start":
+		pr = subprocess.Popen(['ping','google.com'], stdout=subprocess.PIPE)
+		return "started"
 	elif action == "stop":
-		st = False
+		pr.kill()
 		return "stopped"
 	else:
-		return JSONresponse(status_code=404,content="")
+		return JSONResponse(status_code=404,content="")

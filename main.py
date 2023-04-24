226192e3b4d989606d86a887a0d8daaade6cdc7c
ale <i@example.com>
2023-04-24 01:20:21 +0000

    New func result{n}

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
-
 import subprocess
 
 app = FastAPI()
-
 pr = subprocess.Popen('true')
 out = [pr.communicate()[0]]
-#subprocess.Popen(['ping', 'google.com'], stdout=subprocess.PIPE)
+	
 
 @app.get("/api/pn")
 def status_proc():
-	global pr
+	global pr, out
 	if pr.poll() is None:
 		return JSONResponse(status_code=200,content="running")
 	else:
@@ -20,15 +18,22 @@ def status_proc():
 
 @app.get("/api/pn/result")
 def result_proc():
-	global pr
+	global pr, out
 	if pr.poll() is not None:
-		out.append(pr.communicate()[0])
-		return JSONResponse(status_code=200, content="out[-1]")
+		return JSONResponse(status_code=200, content=out[-1])
 	return JSONResponse(status_code=404,content="")
+
+@app.get("/api/pn/result/{n}")
+def result_procNum(n: int):
+	global pr, out
+	if n > 0 and n < len(out):
+		return JSONResponse(status_code=200,content=out[n])
+	else:
+		return JSONResponse(status_code=404,content="")
 
 @app.post("/api/pn")
 def start_proc(action: str):
-	global pr
+	global pr, out
 	if action == "start" and pr.poll() is None:
 		return JSONResponse(status_code=200,content="already started")
 	elif action == "start":
@@ -36,6 +41,7 @@ def start_proc(action: str):
 		return JSONResponse(status_code=200,content="started")
 	elif action == "stop":
 		pr.kill()
+		out.append(pr.communicate()[0])
 		return JSONResponse(status_code=200,content="stopped")
 	else:
 		return JSONResponse(status_code=404,content="")
